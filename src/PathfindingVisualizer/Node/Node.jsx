c85be84d0bf9cfc0064cd1d36efc6491059df441
RishikeshPatil20 <rishiofficial1820@gmail.com>
2023-08-11 18:13:10 +0530

    Basic Code Ready

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -1,13 +1,36 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import './Node.css';
+import "./Node.css";
 
 export default class Node extends Component {
-    constructor(props){
-       super(props);
-       this.state = {}; 
-    }
-    render() {
-        return <div className="node"></div>
-    }
+  render() {
+    const {
+      col,
+      isFinish,
+      isStart,
+      isWall,
+      onMouseDown,
+      onMouseEnter,
+      onMouseUp,
+      row,
+    } = this.props;
+    const extraClassName = isFinish
+      ? "node-finish"
+      : isStart
+      ? "node-start"
+      : isWall
+      ? "node-wall"
+      : "";
+
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        //Down this are component to handle mouse ,that is to make a wall.
+        onMouseDown={() => onMouseDown(row, col)} // just when we press
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}
+      ></div>
+    );
+  }
 }
